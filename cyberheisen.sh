995da318130281c4403eb9fe7a76e921b6eaf71f
cyberheisen <62576899+cyberheisen@users.noreply.github.com>
2021-10-16 16:44:18 -0500

    Update cyberheisen.sh

--- cyberheisen.sh
+++ cyberheisen.sh
@@ -6,6 +6,7 @@
 USER="kali"
 HOME="/home/$USER"
 SETUPFOLDER="$HOME/Downloads/cyberheisen"
+WEBSERVER="/var/www/server"
 
 
 # We need to create a few folders
@@ -20,7 +21,13 @@ sudo DEBIAN_FRONTEND=noninteractive apt -y -q upgrade
 
 # install tools through apt
 printf "Installing software packages through apt\n"
-sudo apt -y -q install rlwrap docker.io mingw-w64 virtualenv xrdp flameshot htop joplin jq gobuster krb5-user python3-pip python3-pylint-common python3-requests python3-scapy python3-venv python-pip-whl
+sudo DEBIAN_FRONTEND=noninteractive sudo apt -y -q install rlwrap docker.io mingw-w64 virtualenv xrdp flameshot htop joplin jq gobuster krb5-user python3-pip python3-pylint-common python3-requests python3-scapy python3-venv python-pip-whl wine64
+
+### Webserver/FTPserver 
+printf "Creating webserver/ftp server folder structure\n"
+sudo mkdir -p $WEBSERVER $WEBSERVER/linux $WEBSERVER/windows $WEBSERVER/transfer
+# Let's make it publicly available
+sudo chmod -R 777 $WEBSERVER
 
 ### install other tools
 
@@ -30,7 +37,10 @@ sudo curl -L https://github.com/carlospolop/PEASS-ng/blob/master/winPEAS/winPEAS
 sudo curl -L https://github.com/carlospolop/PEASS-ng/blob/master/winPEAS/winPEASexe/binaries/x86/Release/winPEASx86.exe -o /usr/share/windows-binaries/winPEASx86.exe
 sudo mkdir /usr/share/linPEAS
 sudo curl -L https://raw.githubusercontent.com/carlospolop/PEASS-ng/master/linPEAS/linpeas.sh -o /usr/share/linPEAS/linpeas.sh
-sudo chmod +x /usr/share/LinPEAS
+sudo chmod +x /usr/share/linPEAS
+# Create links to the webserver
+sudo ln -s /usr/share/linPEAS/linpeas.sh /var/www/server/linux/linpeas.sh
+
 
 #### Penelope Shell Handler ####
 printf "Installing Penelope Shell Handler\n"
@@ -41,6 +51,11 @@ chmod +x /sbin/penelope.py
 #### Evil-WinRM
 sudo gem install evil-winrm
 
+### Web Server Links
+printf "Creating additional necessary file links for the webserver\n"
+ln -s /bin/nc $WEBSERVER/linux/nc
+ln -s /usr/share/windows-binaries $WEBSERVER/windows/tools
+
 ### System Configurations
 
 ### Firefox 
@@ -50,12 +65,12 @@ printf "Installing FoxyProxy with localhost:8080 proxy configuration\n"
 sudo curl -L https://addons.mozilla.org/firefox/downloads/file/3616824/foxyproxy_standard-7.5.1-an+fx.xpi --output /usr/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/foxyproxy@eric.h.jung.xpi
 printf "FoxyProxy Installed"
 
-# Configure Firefox
+### Configure Firefox
 printf "Configuring Firefox\n"
 sudo curl -L https://github.com/cyberheisen/kali_cyberheisen_edition/raw/main/resources/mozilla_settings.7z --output ./mozilla_settings.7z
 7z x $SETUPFOLDER/mozilla_settings.7z -o$HOME/ -aoa
 
-# update .zshrc
+### update .zshrc
 printf "Configuring Zshell\n"
 printf "backup original Zshell configuration files\n"
 mv $HOME/.zshrc .zshrc.orig.bak
@@ -65,9 +80,14 @@ curl https://raw.githubusercontent.com/cyberheisen/kali_cyberheisen_edition/main
 source $HOME/.zshrc
 sudo cp $HOME/.zshrc /root/.zshrc
 
+### Wordlists
 # unzip rockyou.txt
 printf "Unzipping rockyou.txt\n"
 sudo gunzip /usr/share/wordlists/rockyou.txt.gz
+
+# OneRuletoRuleThemAll
+printf "Downloading One Rule to Rule them All\n"
+sudo curl -L https://raw.githubusercontent.com/NotSoSecure/password_cracking_rules/master/OneRuleToRuleThemAll.rule -o /usr/share/hashcat/rules/OneRuleToRuleThemAll.rule
 
 ### configure ssh
 printf "Configuring SSH\n"
